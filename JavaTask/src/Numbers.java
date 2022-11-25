77425dd9a95e975ef54d06d574dd1e816fc350b3
rlek1n <manhax94@gmail.com>
2022-11-25 13:03:22 +0300

    remove multipleOf3 method

--- JavaTask/src/Numbers.java
+++ JavaTask/src/Numbers.java
@@ -3,11 +3,4 @@ public class Numbers {
         if (num > 7)
             System.out.println("Привет");
     }
-
-    public static void multipleOf3(int[] array) {
-        for (int i : array) {
-            if (i % 3 == 0)
-                System.out.print(i + " ");
-        }
-    }
 }
